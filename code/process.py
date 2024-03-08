685c96a6bcce2346ccd750182ef9282e4235dde6
Karnik Kanojia <karnikk1406120@gmail.com>
2024-03-08 22:08:21 +0530

    Fix: URL changed to blob name in

--- code/process.py
+++ code/process.py
@@ -5,22 +5,24 @@ from PIL import Image
 import torchvision
 
 
-def bytes_to_pil(img_bytes) -> np.ndarray:
+def bytes_to_pil(img_bytes: bytes) -> np.ndarray:
     image = Image.open(io.BytesIO(img_bytes))
     return image
 
 
 def preprocess(img):
-    transform = torchvision.transforms.Compose([
-        torchvision.transforms.Grayscale(num_output_channels=1),
-        torchvision.transforms.CenterCrop(1024),
-        torchvision.transforms.Resize((224,224)),
-        torchvision.transforms.ToTensor(),
-    ])
+    transform = torchvision.transforms.Compose(
+        [
+            torchvision.transforms.Grayscale(num_output_channels=1),
+            torchvision.transforms.CenterCrop(1024),
+            torchvision.transforms.Resize((224, 224)),
+            torchvision.transforms.ToTensor(),
+        ]
+    )
     transformed_output = transform(img)
 
     img_min = transformed_output.min()
     img_max = transformed_output.max()
 
-    rescaled_output = 2048*(transformed_output-img_min)/(img_max-img_min) - 1024
+    rescaled_output = 2048 * (transformed_output - img_min) / (img_max - img_min) - 1024
     return transformed_output, rescaled_output
--- code/score.py
+++ code/score.py
@@ -13,10 +13,12 @@ from torchcam.utils import overlay_mask
 from azure.storage.blob import BlobServiceClient, ContentSettings
 from process import bytes_to_pil, preprocess
 import dotenv
+
 dotenv.load_dotenv()
 
 logger = logging.getLogger(__name__)
 
+# Methods available in torchcam
 METHODS = {
     "gradcam": methods.GradCAM,
     "scorecam": methods.ScoreCAM,
@@ -44,19 +46,23 @@ def init():
     model.float()
     if torch.cuda.is_available():
         model = model.cuda()
-    model.eval()
+    model.eval()  # Set model to evaluation mode for inference
     logger.info(f"{model.weights}: Loaded model from path: {path}")
     # Connect to the blob storage
     blob_service_client = BlobServiceClient(
-        account_url=f"https://{account_name}.blob.core.windows.net/",
-        credential=os.getenv("AZURE_STORAGE_ACCESS_KEY")
+        account_url=f"https://{account_name}.blob.core.windows.net",
+        credential=os.getenv("AZURE_STORAGE_ACCESS_KEY"),
     )
     container_client = blob_service_client.get_container_client(
         os.getenv("AZURE_STORAGE_CONTAINER_NAME")
     )
     logger.info(f"{model.weights}: Connected to blob storage")
     method = os.getenv("METHOD", "gradcam")
-    target_layer = model.features.get_submodule(os.getenv("TARGET_LAYER", "denseblock4.denselayer16.conv2"))
+
+    # Get the target layer for the CAM, layers can be grouped into blocks and called
+    target_layer = model.features.get_submodule(
+        os.getenv("TARGET_LAYER", "denseblock4.denselayer16.conv2")
+    )
     cam = METHODS[method](model=model, target_layer=target_layer)
 
 
@@ -67,19 +73,27 @@ def run(raw_data):
     method and return the result back
     """
     logger.info(f"{model.weights}: Request received")
-    results = {}
+
+    # Initialize the response dictionary
+    response = {}
+
+    # Get the uuid for the image from the request
     image_uuid = json.loads(raw_data)["image_uuid"]
+
+    # Get the image from the blob storage
     blob_client = container_client.get_blob_client(image_uuid)
     byte_data = blob_client.download_blob().readall()
+
+    # Converting the bytes of image to PIL image
     data = bytes_to_pil(byte_data)
     transformed, rescaled = preprocess(data)
-    results["predictions"], preds = inference(rescaled)
-    results["gradimages"] = get_gradcam(
-        transformed, preds, image_uuid
-    )
+
+    # Get the predictions and gradcam images
+    response["predictions"], preds = inference(rescaled)
+    response["gradimages"] = get_gradcam(transformed, preds, image_uuid)
     logger.info(f"{model.weights}: Request processed")
-    results = json.dumps(results)
-    return results
+    response = json.dumps(response)
+    return response
 
 
 def inference(image: torch.tensor) -> tuple[dict, torch.Tensor]:
@@ -89,16 +103,23 @@ def inference(image: torch.tensor) -> tuple[dict, torch.Tensor]:
     Returns:
 `       tuple[dict, torch.Tensor]: Dictionary of predictions for HTTP response and tensor of predictions
     """
+
+    # Add a batch dimension to the image tensor
     image = image.unsqueeze(0)
     if torch.cuda.is_available():
         image = image.cuda()
 
+    # Get the predictions
     preds = model(image).cpu()
+
+    # Convert the predictions to a dictionary
     preds_dict = dict(zip(model.pathologies, preds[0].detach().numpy().tolist()))
     return preds_dict, preds
 
 
-def get_gradcam(transformed: torch.Tensor, preds: torch.Tensor, image_uuid: str) -> dict:
+def get_gradcam(
+    transformed: torch.Tensor, preds: torch.Tensor, image_uuid: str
+) -> dict:
     """
     Args:
         transformed: Image after preprocessing
@@ -107,14 +128,24 @@ def get_gradcam(transformed: torch.Tensor, preds: torch.Tensor, image_uuid: str)
     Returns:
         dict: Dictionary of gradcam images URLs for each pathology in blob storage
     """
+
+    # Settings for the blob storage for image as PNG format
     content_settings = ContentSettings(content_type="image/png")
+
+    # Reduces the gradients to zero
     model.zero_grad()
     outputs = {}
+
+    # Gradcam for each pathology
     cam_extractors = [
-        cam(class_idx=i, scores=preds, retain_graph=True) for i, _ in enumerate(model.pathologies)
+        cam(class_idx=i, scores=preds, retain_graph=True)
+        for i, _ in enumerate(model.pathologies)
     ]
+
     for i, pathology in enumerate(model.pathologies):
         activation_maps = cam_extractors[i]
+
+        # Overlay the mask on the image and save it to the blob storage
         activation_maps = (
             activation_maps[0]
             if len(activation_maps) == 1
@@ -126,12 +157,15 @@ def get_gradcam(transformed: torch.Tensor, preds: torch.Tensor, image_uuid: str)
             alpha=0.7,
         )
         buffer = io.BytesIO()
-        result.save(buffer, format='PNG')
+        result.save(buffer, format="PNG")
         buffer.seek(0)
-        blob_client = container_client.get_blob_client(
-            f"{image_uuid}_{pathology}"
+        blob_client = container_client.get_blob_client(f"{image_uuid}_{pathology}")
+        blob_client.upload_blob(
+            buffer,
+            overwrite=True,
+            blob_type="BlockBlob",
+            content_settings=content_settings,
         )
-        blob_client.upload_blob(buffer, overwrite=True, blob_type="BlockBlob", content_settings=content_settings)
         logger.info(f"Uploaded {blob_client.blob_name} to blob storage")
-        outputs[pathology] = blob_client.url
+        outputs[pathology] = blob_client.blob_name
     return outputs
